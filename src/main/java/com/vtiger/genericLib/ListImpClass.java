faa0fad626faef491677a88b723f9e0aabcf216e
kailash <kailash007ranchi@gmail.com>
2020-01-03 09:01:06 +0530

    first commit

---
+++ src/main/java/com/vtiger/genericLib/ListImpClass.java
@@ -0,0 +1,73 @@
+package com.vtiger.genericLib;
+
+import java.io.File;
+import java.util.Date;
+
+import org.apache.commons.io.FileUtils;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.support.events.EventFiringWebDriver;
+import org.testng.ITestContext;
+import org.testng.ITestListener;
+import org.testng.ITestResult;
+
+
+public class ListImpClass implements ITestListener {
+
+	@Override
+	public void onTestStart(ITestResult result) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void onTestSuccess(ITestResult result) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void onTestFailure(ITestResult result) {
+		Date date=new Date();
+		String currentDate=date.toString().replace(":", "_").replace(" ", "_");
+		
+		String failedTestName=result.getMethod().getMethodName();
+		System.out.println("--------failed----"+failedTestName);
+		EventFiringWebDriver eDriver=new EventFiringWebDriver(BaseClass.driver);
+		File srcFile=eDriver.getScreenshotAs(OutputType.FILE);
+		File destFile=new File("./screenshot/"+failedTestName+"_"+currentDate+".png");
+		try{
+			FileUtils.copyFile(srcFile, destFile);
+			
+		}
+		catch (Exception e) {
+			// TODO: handle exception
+			System.out.println("wxeception");
+		}
+		
+	}
+
+	@Override
+	public void onTestSkipped(ITestResult result) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void onTestFailedButWithinSuccessPercentage(ITestResult result) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void onStart(ITestContext context) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void onFinish(ITestContext context) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
